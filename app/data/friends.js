56e58d87025dce906c812ecdb8d4e63ac4eb23d3
Kung Fu Kenny <brianferreira74@yahoo.com>
2019-05-16 15:16:18 -0400

    created a data object

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+[
+    {
+        "name": "Kendrick Lamar",
+        "photo": "https://imgur.com/gallery/O32QW51",
+        "scores": [
+            "3",
+            "2",
+            "5",
+            "5",
+            "1",
+            "4",
+            "2",
+            "3",
+            "1",
+            "4"
+        ]
+    }
+]
