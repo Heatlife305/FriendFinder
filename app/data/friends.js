575cbac7af0cff284bb5de1592bec92d6240dfb6
Kung Fu Kenny <brianferreira74@yahoo.com>
2019-05-17 18:51:30 -0400

    api now has 9 objects in the data array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 [
     {
         "name": "Kendrick Lamar",
-        "photo": "https://imgur.com/gallery/O32QW51",
+        "photo": "https://usercontent2.hubstatic.com/13677643_f520.jpg",
         "scores": [
             "3",
             "2",
@@ -14,5 +14,134 @@
             "1",
             "4"
         ]
+    },
+    {
+        "name": "LeBron James",
+        "photo": "https://moneydotcomvip.files.wordpress.com/2018/07/180702-lebron-james-lakers.jpg?quality=85&w=1012&h=569&crop=1",
+        "scores": [
+            "1",
+            "2",
+            "4",
+            "1",
+            "5",
+            "4",
+            "3",
+            "3",
+            "2",
+            "5"
+        ]
+    },
+    {
+        "name": "Bruce Banner",
+        "photo": "https://cdn.vox-cdn.com/thumbor/wgMcgj6LStdjW-qlLkaHUBsdQzY=/0x0:2048x858/1200x800/filters:focal(834x251:1160x577)/cdn.vox-cdn.com/uploads/chorus_image/image/57442421/hulk_agnarok.0.jpg",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "3",
+            "3",
+            "1",
+            "4",
+            "1",
+            "2",
+            "2"
+        ]
+    },
+    {
+        "name": "Aaron Rodgers",
+        "photo": "https://pixel.nymag.com/imgs/daily/vulture/2019/05/13/13-aaron-rodgers.w330.h330.jpg",
+        "scores": [
+            "3",
+            "2",
+            "5",
+            "4",
+            "4",
+            "2",
+            "3",
+            "1",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Jason Voorhees",
+        "photo": "https://i.ytimg.com/vi/O-KRot-Ju4E/maxresdefault.jpg",
+        "scores": [
+            "1",
+            "2",
+            "1",
+            "1",
+            "2",
+            "3",
+            "1",
+            "2",
+            "4",
+            "2"
+        ]
+    },
+    {
+        "name": "Daenerys Targaryen",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLSz_0kwkuqUHY2ugG9_TP5aYrjhlkDbJoxHKmjOO7fJvhAAMqrg",
+        "scores": [
+            "2",
+            "5",
+            "3",
+            "1",
+            "5",
+            "4",
+            "2",
+            "3",
+            "1",
+            "5"
+        ]
+    },
+    {
+        "name": "Robyn Fenty",
+        "photo": "https://www.rap-up.com/app/uploads/2019/05/rihanna-dior.jpg",
+        "scores": [
+            "4",
+            "4",
+            "1",
+            "1",
+            "2",
+            "3",
+            "5",
+            "3",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Leonardo DiCaprio",
+        "photo": "https://pixel.nymag.com/imgs/daily/vulture/2018/09/17/17-leonardo-dicaprio.w330.h330.jpg",
+        "scores": [
+            "5",
+            "4",
+            "5",
+            "4",
+            "4",
+            "4",
+            "3",
+            "5",
+            "1",
+            "5"
+        ]
+    },
+    {
+        "name": "Patrick Star",
+        "photo": "https://media0dk-a.akamaihd.net/87/16/2cda3e87c8d45e18cc7e3e931c26a244.jpg",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "2",
+            "1",
+            "1",
+            "2",
+            "3",
+            "2"
+        ]
     }
+    
 ]
