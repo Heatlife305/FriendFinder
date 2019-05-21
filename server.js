563aff6e56bc26c97157441fcd7696e9c2945fdc
Kung Fu Kenny <brianferreira74@yahoo.com>
2019-05-21 12:28:56 -0400

    able to get a reponse from the server with GET routes to survey page or home page

--- server.js
+++ server.js
@@ -6,8 +6,17 @@ let = path = require("path");
 // Sets up the Express App
 // =============================================================
 let = app = express();
-let = PORT = 4000;
+let = PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static('app/public'));
+
+//Router
+//require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
+
+//Listening to the port that was set up
+app.listen(PORT, () => console.log("Listening on port %s", PORT));
